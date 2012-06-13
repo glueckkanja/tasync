f25161b2d959c809311617f0567e950b4f221315
jpberdel <jp.berdel@googlemail.com>
2012-06-13 10:34:09 +0200

    added mono.options

--- Program.cs
+++ Program.cs
@@ -1,14 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Mono.Options;
 
-namespace azure_table_storage_sync
+namespace Tasync
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static int Main(string[] args)
         {
+            var cmd = new Dictionary<string, string>();
+
+            var p = new OptionSet
+                        {
+                            {"h|help", "show this message and exit", s => cmd["help"] = s}
+                        };
+
+            try
+            {
+                p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.WriteLine("Invalid arguments");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try 'tysync --help'.");
+                return 1;
+            }
+
+            if (cmd.ContainsKey("help"))
+            {
+                ShowHelp(p);
+                return 0;
+            }
+
+            ShowHelp(p);
+            return 0;
+        }
+
+        private static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("tasync");
+            Console.WriteLine("Export, sync and import whole Azure Table Storage accounts.");
+            Console.WriteLine();
+
+            p.WriteOptionDescriptions(Console.Out);
         }
     }
 }
