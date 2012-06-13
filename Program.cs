ac84e6802c5521d6f5b889d381cb47e6024ea3f2
jpberdel <jp.berdel@googlemail.com>
2012-06-13 10:09:29 +0200

    gh4w

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace azure_table_storage_sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
